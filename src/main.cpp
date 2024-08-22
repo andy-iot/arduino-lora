7dcabfab2165272ab852a5b0c93b2566428b2f4a
saarmstrong <andy.armstrong1228@gmail.com>
2024-08-21 21:59:34 -0700

    added libpax demo

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 #include <BME280I2C.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
-
+#include <libpax_api.h>
 
 #define SCK     5    // GPIO5  -- SCK
 #define MISO    19   // GPIO19 -- MISO
@@ -17,7 +17,6 @@
 #define RST     23   // GPIO14 -- RESET (If Lora does not work, replace it with GPIO14)
 #define DI0     26   // GPIO26 -- IRQ(Interrupt Request)
 #define BAND    433E6
-
 
 unsigned int counter = 0;
 
@@ -39,6 +38,29 @@ BME280I2C::Settings settings(
 
 BME280I2C bme(settings);
 Adafruit_MPU6050 mpu;
+
+struct count_payload_t count_from_libpax;
+
+void process_count(void) {
+  printf("pax: %lu; %lu; %lu;\n", count_from_libpax.pax, count_from_libpax.wifi_count, count_from_libpax.ble_count);
+}
+
+void initPaxCount() {
+  struct libpax_config_t configuration; 
+  libpax_default_config(&configuration);
+  configuration.blecounter = 1;
+  configuration.blescantime = 0; // infinit
+  configuration.wificounter = 1; 
+  configuration.wifi_channel_map = WIFI_CHANNEL_ALL;
+  configuration.wifi_channel_switch_interval = 5;
+  configuration.wifi_rssi_threshold = -80;
+  configuration.ble_rssi_threshold = -80;
+  libpax_update_config(&configuration);
+
+  // internal processing initialization
+  libpax_counter_init(process_count, &count_from_libpax, 10, 1); 
+  libpax_counter_start();
+}
 
 void static updateOled(int counter)
 {
@@ -144,13 +166,15 @@ void setup() {
   }
   Serial.println("MPU6050 Found!");
 
-  //setupt motion detection
+  //setup motion detection
   mpu.setHighPassFilter(MPU6050_HIGHPASS_0_63_HZ);
   mpu.setMotionDetectionThreshold(1);
   mpu.setMotionDetectionDuration(20);
   mpu.setInterruptPinLatch(true);	// Keep it latched.  Will turn off when reinitialized.
   mpu.setInterruptPinPolarity(true);
   mpu.setMotionInterrupt(true);
+
+  initPaxCount();
 
   display.init();
   display.flipScreenVertically();  
